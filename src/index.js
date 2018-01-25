96dacbe877803ec9036c4a3577464f20055f101a
Other Kristine <kristina.lebedeva@inbox.lv>
2018-01-25 14:39:52 +0200

    Adding component Header

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import Header from './components/header';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+    return (
+        <div> 
+            <Header />
+        </div>
+        );
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
