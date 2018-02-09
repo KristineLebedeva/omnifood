68945d01f2bf984ffd9f70f8434349de8a8f7867
Other Kristine <kristina.lebedeva@inbox.lv>
2018-02-09 14:15:37 +0200

    Creating Header and cleaned projects structure

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Header = () => {
-    return (<div> Header </div>);
+    return (
+        <div className="hero-text-box">
+            <h1>Googbye junk food. Hello super healthy meals.</h1>
+            <a href="#">I'm hungry</a>
+            <a href="#">Show me more</a>
+        </div>
+    );
 };
             
 export default Header;
